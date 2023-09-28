36dc2034050210d8535a638cd8ebacde81bae02e
Ani <anisingh@outlook.com>
2023-09-28 22:34:32 +0100

    Add terms and conditions checkbox and link

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,12 +6,23 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  validate :age_must_be_over_13
+  validate :age_must_be_over_13,
 
   private
 
   def age_must_be_over_13
     errors.add(:base, 'Age must be over 13 to join') unless isAgeOver13
   end
-       
+
+  def agreed_to_terms
+    unless agreed_to_terms?
+      errors.add(:agreed_to_terms, 'Must agree to terms and conditions')
+    end
+  end
+
+  def agreed_to_terms?
+    # Check if the agreed_to_terms checkbox is checked
+    agreed_to_terms.present? && agreed_to_terms == '1'
+  end
 end
+
