65dd6807265e82b624bc7f733fe11a52fef53916
Ani <anisingh@outlook.com>
2023-09-29 11:17:26 +0100

    Update terms.html page to nice format

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,7 +6,8 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   
-  validate :age_must_be_over_13,
+  validate :age_must_be_over_13
+  validate :user_must_agree_to_terms
 
   private
 
@@ -14,15 +15,9 @@ class User < ApplicationRecord
     errors.add(:base, 'Age must be over 13 to join') unless isAgeOver13
   end
 
-  def agreed_to_terms
-    unless agreed_to_terms?
-      errors.add(:agreed_to_terms, 'Must agree to terms and conditions')
-    end
+  def user_must_agree_to_terms
+      errors.add(:base, 'Must agree to terms and conditions') unless agreed_to_terms
   end
 
-  def agreed_to_terms?
-    # Check if the agreed_to_terms checkbox is checked
-    agreed_to_terms.present? && agreed_to_terms == '1'
-  end
 end
 
